2ef5cc81a04929dd010e102f5a4a9fe42e1f66ea
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-10-25 12:22:16 -0300

    Update login.py

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -1,10 +1,1 @@
-"""Login schemas implementation."""
 
-from pydantic import BaseModel, EmailStr
-
-
-class LoginSchema(BaseModel):
-    """Login schema."""
-
-    username: EmailStr
-    password: str

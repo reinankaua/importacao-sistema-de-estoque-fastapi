34d4de12b0540fdab661b5bb76cb814048c45f7d
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-10-25 12:47:21 -0300

    Update database.py

--- app/flastapi_app/database.py
+++ app/flastapi_app/database.py
@@ -5,3 +5,5 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+uvicorn fastapi_app.main:app --reload

c278b9111530aaf0c126d80e3ffbcc067da91d36
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-10-28 18:30:39 -0300

    Create models.py

---
+++ app/utils/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    endereco = db.Column(db.String(200))
+    contato = db.Column(db.String(50))

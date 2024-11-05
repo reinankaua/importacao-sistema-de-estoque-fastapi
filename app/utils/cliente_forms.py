34af238f03ef0d5cb0d2b0d8dd1e0bcbf952493d
Reinan Kauã <contato.reinankaua@gmail.com>
2024-11-05 09:51:33 -0300

    feat: removendo arquivos desnecessarios

--- app/utils/cliente_forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class ClienteForm(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired()])
-    endereco = StringField('Endereço')
-    contato = StringField('Contato')
-    submit = SubmitField('Salvar')
--- app/utils/common.py
+++
@@ -1,6 +0,0 @@
-def real_to_float(real: str) -> float:
-    real = real.replace('R$', '')
-    real = real.replace('.', '')
-    real = real.replace(' ', '')
-    real = real.replace(',', '.')
-    return float(real)
